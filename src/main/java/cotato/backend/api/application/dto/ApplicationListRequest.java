b4a3c3555bc9f02e594efd792c2ce9b8bacd30cc
Junseung-Ock <junseung0305@kyonggi.ac.kr>
2026-05-14 17:19:29 +0900

    fix: GET 서류 목록 조회를 @RequestBody에서 @ModelAttribute로 변경

--- src/main/java/cotato/backend/api/application/dto/ApplicationListRequest.java
+++ src/main/java/cotato/backend/api/application/dto/ApplicationListRequest.java
@@ -1,8 +1,10 @@
 package cotato.backend.api.application.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ApplicationListRequest {
     private String filterBy;  // "likes", "gisu", "gisu+likes"
     private int period;
